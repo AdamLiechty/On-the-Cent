f67c6efc670bc8e5d93748d805ee1fd2cd4fcebb
Adam Liechty <adam.liechty@gmail.com>
2013-10-26 12:59:35 -0700

    Trail and admin codes

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+var fs = require("fs");
+
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+
+  app.set("dataDir", "/Users/adamli/OnTheCent_Data");
+
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -31,6 +36,11 @@ app.get("/", routes.index);
 app.get("/create-trail", routes.createTrail)
 app.get("/users", user.list);
 
+fs.mkdir(app.get("dataDir"), function(ex) {
+  if (ex && ex.code != "EEXIST")
+    throw ex;
+});
+
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("On-the-Cent server listening on port " + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,51 @@
-
-/*
- * GET home page.
- */
+var crypto = require("crypto"),
+    querystring = require("querystring");
 
 exports.index = function(req, res){
   res.render("index", { title: "On the Cent" });
 };
 
 exports.createTrail =  function(req, res){
-  res.render("create-trail", { title: "On the Cent" });
+
+  if (!req.query.trail) {
+    crypto.randomBytes(32, function(ex, buf) {
+      if(ex) throw ex;
+      crypto.randomBytes(32, function(ex2, buf2) {
+        if(ex2) throw ex2;
+
+        var trail = buf.toString("base64");
+        var admin = buf2.toString("base64");
+
+        //go(trail, admin);
+        res.redirect("/create-trail?" + getAdminQuery(trail, admin));
+      });
+    });
+  } else {
+    console.log(req.query);
+    go(req.query.trail, req.query.admin);
+  }
+
+  function getAdminQuery(trail, admin) {
+    return querystring.stringify({
+      admin: admin,
+      trail: trail
+    });    
+  }
+
+  function go(trail, admin) {
+    var adminQuery = getAdminQuery(trail, admin);
+
+    var trailQuery = querystring.stringify({
+      trail: trail
+    });
+
+    res.render("create-trail", {
+      title: "On the Cent",
+      pennies: [{year: 1234}],
+      trail: trail,
+      admin: admin,
+      adminQuery: adminQuery,
+      trailQuery: trailQuery
+    });
+  }
 };
