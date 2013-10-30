d17e9c039e95112e64c65d5f606c838cd7c9358c
Adam Liechty <adam.liechty@gmail.com>
2013-10-30 08:52:03 -0700

    Tap on year digits

--- public/javascripts/trail.js
+++ public/javascripts/trail.js
@@ -10,11 +10,25 @@ function Trail() {
   self.data = ko.observable();
   self.started = ko.observable(false);
   self.index = ko.observable(0);
+  self.year = ko.observable("");
 
   self.get = function() {
     $.get("/api/trails/" + _trailId, function(data, status, jqXHR) {
       self.data(data);
     });
+  };
+
+  self.tap = function(n) {
+    return function() {
+      if (self.year().length < 4)
+        self.year(self.year() + n);
+    };
+  };
+
+  self.yearDigitImage = function(i) {
+    if (self.year().length > i)
+      return "images/" + self.year()[i] + ".jpg";
+    return null;
   };
 
   self.start = function() {
